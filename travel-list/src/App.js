07a6b252ab5976c00c9b2817301887d65b0454c4
Anish Gharti <anishgc@proton.me>
2025-07-11 17:07:02 +0545

    understood difference between state and props

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,10 +21,30 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e){
+
+    e.preventDefault();
+    if (!description) return;
+    const newItem = {description, quantity, packed: false, id: Date.now()};
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>
+        {Array.from({length: 20}, (_, i) => (
+          <option key={i} value={i +1}>{i+1}</option>
+          
+        ))}
+      </select>
+
+      <input type="text" placeholder="Item..." value={description} onChange={(e)=> setDescription(e.target.value)}></input>
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +53,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -40,11 +62,12 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity}
         {item.description}
       </span>
-      <button> &times;</button>
+
+      <button>❌ </button>
     </li>
   );
 }
